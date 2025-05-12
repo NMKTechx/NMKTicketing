c785187695ebb51c94cfe2f50f30ad7b44f5592f
histerdans <40757754+histerdans@users.noreply.github.com>
2025-05-12 11:01:07 +0300

    Update create_superuser.py

--- scripts/create_superuser.py
+++ scripts/create_superuser.py
@@ -15,9 +15,13 @@ User = get_user_model()
 SUPERUSER_USERNAME = os.getenv("SUPERUSER_USERNAME", "admin")
 SUPERUSER_EMAIL = os.getenv("SUPERUSER_EMAIL", "admin@example.com")
 SUPERUSER_PASSWORD = os.getenv("SUPERUSER_PASSWORD", "123456l7")
+SUPERUSER_EMPLOYEE_NAME = os.getenv("SUPERUSER_EMPLOYEE_NAME", "Admin User")
+SUPERUSER_DEPARTMENT = os.getenv("SUPERUSER_DEPARTMENT", "IT")
 
 if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
     User.objects.create_superuser(
+        employee_name=SUPERUSER_EMPLOYEE_NAME,
+        department=SUPERUSER_DEPARTMENT,
         username=SUPERUSER_USERNAME,
         email=SUPERUSER_EMAIL,
         password=SUPERUSER_PASSWORD
